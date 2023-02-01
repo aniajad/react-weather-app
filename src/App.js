0da8cee07fd11e3d43c99319addeeb2b1b6272a5
aniajad <116589679+aniajad@users.noreply.github.com>
2023-02-01 20:54:12 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        <a href="https://github.com/aniajad/react-weather-app" target="_blank">
-          Open Source on GitHub{" "}
-        </a>{" "}
-        by Anna Jadczyszyn
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          <a
+            href="https://github.com/aniajad/react-weather-app"
+            target="_blank"
+          >
+            Open Source on GitHub{" "}
+          </a>{" "}
+          by Anna Jadczyszyn
+        </footer>
+      </div>
     </div>
   );
 }
