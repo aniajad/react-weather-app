d3d764d947bd2a7319b484960d9b1552a300eeb7
aniajad <116589679+aniajad@users.noreply.github.com>
2023-02-01 22:13:10 +0100

    Added styling to the project

--- src/Weather.js
+++ src/Weather.js
@@ -10,7 +10,7 @@ export default function Weather() {
             <input
               type="submit"
               value="Your city"
-              className="btn btn-primary"
+              className="btn btn-primary w-100"
             />
           </div>
           <div className="col-6">
@@ -18,10 +18,15 @@ export default function Weather() {
               type="search"
               placeholder="Type a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -30,14 +35,17 @@ export default function Weather() {
         <li>Wednesday 21:00</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
           <img
             src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-night.png"
             alt="Mostly Cloudy"
           />
-          7°C
+
+          <span className="degrees">7</span>
+          <span className="unit">°C</span>
         </div>
+
         <div className="col-6">
           <ul>
             <li>Feels like: 3°C</li>
@@ -46,7 +54,6 @@ export default function Weather() {
           </ul>
         </div>
       </div>
-      Hello from Weather
     </div>
   );
 }
